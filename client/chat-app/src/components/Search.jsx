131a049273f1a27124c78612befdf5a1aea2640f
Anushka gupta <anushkagupta319@gmail.com>
2024-01-28 22:51:36 +0530

    search user completed

--- client/chat-app/src/components/Search.jsx
+++ client/chat-app/src/components/Search.jsx
@@ -1,13 +1,16 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 //import user from "../img/user.png"
-import { collection,  getDocs, query, where } from "firebase/firestore";
+import { collection, getDoc, query, serverTimestamp, where,doc,setDoc, getDocs } from "firebase/firestore";
 import {db} from "../firebase"
-
+import { AuthContext } from '../context/AuthContext';
+import { updateDoc } from "firebase/firestore";
 const Search = () => {
 
   const [username, setUsername]=useState("");
   const [user, setUser] = useState(null);
   const [err , setErr] = useState(false); 
+
+  const {currentUser}=useContext(AuthContext);
 
   const handleSearch =async () => {
     setErr(false); 
@@ -34,14 +37,53 @@ const Search = () => {
   };
 
 
-  const handleKey= async ( )=> {
+  const handleKey=  ( e )=> {
     e.code === "Enter" && handleSearch();
   }
 
-  const handleSelect = ( e )=> {
+  const handleSelect = async ( )=> {
     //check if group(chats) exists
 
+    const combinedId = 
+    currentUser.uid > user.uid 
+    ? currentUser.uid + user.uid 
+    : user.uid + currentUser.uid;
 
+    try 
+    {
+      const res= await getDoc(doc(db, "chats", combinedId));
+
+      if(!res.exists()){
+        //create a new chat
+        await setDoc(doc(db, "chats", combinedId),{messages : []});
+
+        //create user chats
+        await updateDoc(doc(db, "userChats", currentUser.uid,),{
+          [combinedId + ".userInfo"] : {
+            uid : user.uid,
+            displayName : user.displayName,
+            photoURL : user.photoURL,
+          },
+          [combinedId + ".date"] : serverTimestamp()
+        })
+
+        //create user chats
+        await updateDoc(doc(db, "userChats", user.uid,),{
+          [combinedId + ".userInfo"] : {
+            uid : currentUser.uid,
+            displayName : currentUser.displayName,
+            photoURL : currentUser.photoURL,
+          },
+          [combinedId + ".date"] : serverTimestamp()
+        })
+      }
+    }
+    catch( err)
+    {
+      console.error('Error creating/updating chat and userChats:', err);
+    }
+    setUser(null);
+    setUsername("")
 
     //create user chats
 
@@ -57,7 +99,7 @@ const Search = () => {
         //listens for input
         onKeyDown={handleKey}
         onChange={(e)=>setUsername(e.target.value)}
-        
+        value={username}
         />
       </div>
       {err && <span>User not found</span>}
